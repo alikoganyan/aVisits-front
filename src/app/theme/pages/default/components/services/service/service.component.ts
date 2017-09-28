d710c191aef4977c6ed5d3a2d0f65a0c069d94d4
alikoganyan <alik.oganyan@gmail.com>
2017-09-28 18:04:54 +0400

    make some logic in create employee page

--- src/app/theme/pages/default/components/services/service/service.component.ts
+++ src/app/theme/pages/default/components/services/service/service.component.ts
@@ -1,6 +1,6 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {ScriptLoaderService} from "../../../../../../_services/script-loader.service";
-import {Message} from "primeng/primeng";
+
 
 @Component({
     selector: 'app-service',
@@ -11,23 +11,12 @@ export class ServiceComponent implements OnInit, AfterViewInit {
 
     constructor(private _script: ScriptLoaderService) { }
 
-    msgs: Message[];
-    index: number = 0;
-    public save;
-    public items;
+
 
     ngOnInit() {
     }
 
-    onTabClose(event) {
-        this.msgs = [];
-        this.msgs.push({ severity: 'info', summary: 'Tab Closed', detail: 'Index: ' + event.index });
-    }
 
-    onTabOpen(event) {
-        this.msgs = [];
-        this.msgs.push({ severity: 'info', summary: 'Tab Expanded', detail: 'Index: ' + event.index });
-    }
 
 
 
--- src/assets/demo/default/custom/components/forms/widgets/dropzone.js
+++ src/assets/demo/default/custom/components/forms/widgets/dropzone.js
@@ -1,1 +1,31 @@
-var DropzoneDemo=function(){var e=function(){Dropzone.options.mDropzoneOne={paramName:"file",maxFiles:1,maxFilesize:5,accept:function(e,o){"justinbieber.jpg"==e.name?o("Naha, you don't."):o()}},Dropzone.options.mDropzoneTwo={paramName:"file",maxFiles:10,maxFilesize:10,accept:function(e,o){"justinbieber.jpg"==e.name?o("Naha, you don't."):o()}},Dropzone.options.mDropzoneThree={paramName:"file",maxFiles:10,maxFilesize:10,acceptedFiles:"image/*,application/pdf,.psd",accept:function(e,o){"justinbieber.jpg"==e.name?o("Naha, you don't."):o()}}};return{init:function(){e()}}}();DropzoneDemo.init();
+var DropzoneDemo = function () {
+    var e = function () {
+        Dropzone.options.mDropzoneOne = {
+            paramName: "img",
+            maxFiles: 1,
+            maxFilesize: 5,
+            headers: {'content-Type' : undefined ,"Cache-Control":undefined},
+            accept: function (e, o) {
+                "justinbieber.jpg" == e.name ? o("Naha, you don't.") : o()
+            }
+        }, Dropzone.options.mDropzoneTwo = {
+            paramName: "img", maxFiles: 10, maxFilesize: 10, accept: function (e, o) {
+                "justinbieber.jpg" == e.name ? o("Naha, you don't.") : o()
+            }
+        }, Dropzone.options.mDropzoneThree = {
+            paramName: "img",
+            maxFiles: 10,
+            maxFilesize: 10,
+            acceptedFiles: "image/*,application/pdf,.psd",
+            accept: function (e, o) {
+                "justinbieber.jpg" == e.name ? o("Naha, you don't.") : o()
+            }
+        }
+    };
+    return {
+        init: function () {
+            e()
+        }
+    }
+}();
+DropzoneDemo.init();
